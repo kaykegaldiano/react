6f15b07f456d0ae2c22dc2ff39258ec09746c5af
Kayke Galdiano <kaykegaldiano@gmail.com>
2023-03-20 15:39:34 -0300

    .

--- src/challenges/levels/level3/cl03/TaskList.jsx
+++ src/challenges/levels/level3/cl03/TaskList.jsx
@@ -1,17 +1,23 @@
-import { ReactComponent as TaskIcon} from './task.svg'
+import { ReactComponent as TaskIcon } from './task.svg'
 import { ReactComponent as BoardIcon } from './board.svg'
+import { ReactComponent as CheckedIcon } from './check.svg'
 import { ReactComponent as UncheckedIcon } from './unchecked.svg'
 import { ReactComponent as RemoveIcon } from './remove.svg'
 import './index.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+let nextId = 0
 
 const AddTask = () => {
     const [task, setTask] = useState([])
-    
+
     const addStorage = (e) => {
         const newTask = e.target.previousSibling.value
-        setTask(prevState => [...prevState, newTask])
-        localStorage.setItem('tasks', JSON.stringify([...task, newTask]))
+        setTask(prevState => [...prevState, {id: nextId++, name: newTask, checked: false}])
+        localStorage.setItem('tasks', JSON.stringify([...task, { id: nextId++, name: newTask, checked: false}]))
+        window.dispatchEvent(new Event('storage'))
+        e.target.previousSibling.value = ''
+        e.target.previousSibling.focus()
     }
 
     return (
@@ -29,16 +35,38 @@ const AddTask = () => {
 }
 
 const TaskBoard = () => {
+    const [value, setValue] = useState(true)
+    
+    function updateComponent() {
+        setValue(!value)
+    }
+
+    useEffect(() => {
+        window.addEventListener('storage', updateComponent)
+    }, [value])
+
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+    const taskItems = tasks.map(task => (
+        <div className='task' key={task.id}>
+            <div>
+                { task.checked ? (
+                    <CheckedIcon />
+                ) : <UncheckedIcon /> }
+                <span>{task.name}</span>
+            </div>
+            <RemoveIcon />
+        </div>
+    ))
+
     return (
         <div className='taskboard'>
             <div className='title'>
                 <BoardIcon />
                 <h2 className='title-text'>Quadro de tarefas</h2>
             </div>
-            <div className='task'>
-                <UncheckedIcon />
-                <span>Reunião com cliente</span>
-                <RemoveIcon />
+            <div className='task-items'>
+                {taskItems}
             </div>
         </div>
     )
